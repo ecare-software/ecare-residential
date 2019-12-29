5b745320be2a23e67faed8ae730ecf697d7552d1
demarcus kennedy <demarcuskennedy95@gmail.com>
2019-12-29 01:51:54 -0600

    test 7

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ app.use(express.static(path.join(__dirname, 'client/build'))); //new
 //test
 app.get("/",(req,res)=>{
   // res.sendFile(path.join(__dirname + "/client/build/index.html"))
-  res.sendFile(path.join("/client/build/index.html"))
+  res.sendFile(path.join(__dirname + "/index.html"))
 })
 
 app.use('/', indexRouter);
