62578e3878f686ee5eec3301866198eecaa28429
demarcus kennedy <demarcuskennedy95@gmail.com>
2019-12-28 18:03:47 -0600

    client done for now

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,31 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import Axios from "axios";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    users: []
+  };
+
+  componentDidMount() {
+    Axios.get("/users").then(res => this.setState({ users: res.data }));
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Users</h1>
+        <ul>
+          {this.state.users.map((user, index) => (
+            <li key={"user-index-" + index}>
+              {user.id} - {user.username}
+            </li>
+          ))}
+        </ul>
+      </div>
+    );
+  }
 }
 
 export default App;
