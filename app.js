f6cd332fe84acad34a3e51f51bdf6fe1bb74a3b2
demarcus kennedy <demarcuskennedy95@gmail.com>
2019-12-29 01:34:06 -0600

    test 3

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ app.use(cookieParser());
 
 app.use(express.static(path.join(__dirname, 'client/build'))); //new
 //test
-app.get("/",(req,res)=>{
+app.get("*",(req,res)=>{
   res.sendFile(path.join(__dirname + "build","index.html"))
 })
 
