7413f016adfd1eae8874d0fefad10d52af8e8728
DeMarcus Kennedy <demarcus@demarcuss-mbp.lan>
2020-09-24 15:46:46 -0500

    everything looks good !
    Signed-off-by: DeMarcus Kennedy <demarcus@demarcuss-mbp.lan>

--- routes/api/email.js
+++ routes/api/email.js
@@ -16,33 +16,31 @@ const router = express.Router();
 const nodemailer = require("nodemailer");
 
 router.post("/:email/:name", (req, res) => {
-    // nodemailer.createTestAccount((err,account)=>{
-    //     const html = `<h3>Contact Details</h3> <br> <p>${req.body.name}</p>`
-    // })
-    var email = req.params.email;
-    var name = req.params.name;
-    // res.json({ success: name});
-    let transporter = nodemailer.createTransport({
-        service:'gmail',
-        auth:{
-            user:"txrcs100@gmail.com",
-            pass:"Success19"
-        }
-    });
-    const mailOptions = {
-        from: 'txrcs100@gmail.com', // sender address
-        to:email,
-        subject: `Hello ${name}, from eCare Residential`, // Subject line
-        html: `<p>Something here for ${name}</p>`// plain text body
-      };
-      transporter.sendMail(mailOptions, function (err, info) {
-        if(err){
-            console.log(err);
-            res.json({ success: true });
-        }
-        else
-            res.json({ success: false });
-     });
+  // nodemailer.createTestAccount((err,account)=>{
+  //     const html = `<h3>Contact Details</h3> <br> <p>${req.body.name}</p>`
+  // })
+  var email = req.params.email;
+  var name = req.params.name;
+  // res.json({ success: name});
+  let transporter = nodemailer.createTransport({
+    service: "gmail",
+    auth: {
+      user: "txrcs100@gmail.com",
+      pass: "Success19",
+    },
+  });
+  const mailOptions = {
+    from: "txrcs100@gmail.com", // sender address
+    to: email,
+    subject: `Hello ${name}, from eCare Residential`, // Subject line
+    html: `<p>Something here for ${name}</p>`, // plain text body
+  };
+  transporter.sendMail(mailOptions, function (err, info) {
+    if (err) {
+      console.log(err);
+      res.json({ success: true });
+    } else res.json({ success: false });
+  });
 });
 
 router.get("/", (req, res) => {
