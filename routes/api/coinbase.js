c8e883166540ecac84a95422dfc271376cca817d
DeMarcus Kennedy <demarcus.kennedy@ibm.com>
2022-02-11 13:26:33 -0600

    testing
    Signed-off-by: DeMarcus Kennedy <demarcus.kennedy@ibm.com>

--- routes/api/coinbase.js
+++ routes/api/coinbase.js
@@ -18,53 +18,21 @@ function rawBody(req, res, next) {
 }
 router.post("/confirm", async (req, res) => {
   try {
-    // const rawBody = req.body;
-    // const signature = req.headers["x-cc-webhook-signature"];
-    // const webhookSecret = "2100833d-fec5-48c0-b671-89f3b309075b";
-    // const event = await Webhook.verifyEventBody(
-    //   rawBody,
-    //   signature,
-    //   webhookSecret
-    // );
-    console.log(req.body);
-
-    // let chargeStatus = { status: "" };
-    // if (event.type === "charge:pending") {
-    //   chargeStatus.status = "pending";
-    //   if (event.data.metadata && event.data.metadata.email) {
-    //     await User.updateOne(
-    //       { email: event.data.metadata.email },
-
-    //     );
-    //   }
-    // }
-    // if (event.type === "charge:confirmed") {
-    //   chargeStatus.status = "confirmed";
-    //   if (event.data.metadata && event.data.metadata.email) {
-    //     await User.updateOne(
-    //       { email: event.data.metadata.email },
-    //       { isCoinbasePaymentCompleted: true, isAccountSetupCompleted: true }
-    //     );
-    //   }
-    // }
-    // if (event.type === "charge:resolved") {
-    //   chargeStatus.status = "confirmed";
-    //   if (event.data.metadata && event.data.metadata.email) {
-    //     await User.updateOne(
-    //       { email: event.data.metadata.email },
-    //       { isCoinbasePaymentCompleted: true, isAccountSetupCompleted: true }
-    //     );
-    //   }
-    // }
-    // if (event.type === "charge:failed") {
-    //   chargeStatus.status = "failed";
-    //   await User.updateOne(
-    //     { email: event.data.metadata.email },
-    //     { isCoinbasePaymentCompleted: false, isAccountSetupCompleted: false }
-    //   );
-    // }
-
-    res.status(200).json(req.body);
+    if (req.body && req.body.event) {
+      const event = req.body.event;
+      if (event.type === "charge:confirmed") {
+        console.log("do user data stuff");
+        // if (event.data.metadata && event.data.metadata.email) {
+        //   await User.updateOne(
+        //     { email: event.data.metadata.email },
+        //     { isCoinbasePaymentCompleted: true, isAccountSetupCompleted: true }
+        //   );
+        // }
+      }
+      res.status(200).json(event);
+    } else {
+      res.status(500).send(`Coinbase Error ${e} - bad request body`);
+    }
   } catch (e) {
     console.log(`Failed confirm pay with coinbase ${e}`);
     res.status(500).send(`Coinbase Error ${e}`);
