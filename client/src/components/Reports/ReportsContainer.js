7e43d88e2ba357139fd31a858d43b027ea5a3fd6
Ty D'Angelo <tydangelo18@gmail.com>
2022-02-12 12:16:50 -0600

    Refactored ReportsContainer component by changing from a class to functional component, implement useState hook, switched logic of conditional from three if statements to an if else if else statement and refactored to ternary.
    Signed-off-by: Ty D'Angelo <tydangelo18@gmail.com>

--- client/src/components/Reports/ReportsContainer.js
+++ client/src/components/Reports/ReportsContainer.js
@@ -1,108 +1,94 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import "./ReportsContainer.css";
 import { isAdminUser } from "../../utils/AdminReportingRoles";
 import { FromReports } from "./FormReports";
 import { TrainingReports } from "./TrainingReports";
 
-class ReportsContainer extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      showForms: false,
-      showTrainings: false,
-    };
-  }
+const ReportsContainer = (props) => {
+  const [{ showForms, showTrainings }, setState] = useState({
+    showForms: false,
+    showTrainings: false,
+  });
 
-  showFormReports = () => {
-    this.setState({ ...this.state, showForms: true, showTrainings: false });
+  const showFormReports = () => {
+    setState({ showForms: true, showTrainings: false });
   };
-  showTrainingReports = () => {
-    this.setState({ ...this.state, showForms: false, showTrainings: true });
+  const showTrainingReports = () => {
+    setState({ showForms: false, showTrainings: true });
   };
-  resetReports = () => {
-    this.setState({ ...this.state, showForms: false, showTrainings: false });
+  const resetReports = () => {
+    setState({ showForms: false, showTrainings: false });
   };
 
-  render() {
-    if (!this.state.showForms && !this.state.showTrainings) {
-      return (
-        <div style={{ marginTop: "50px" }}>
-          <div className="row" style={{ margin: "0px 30px" }}>
-            <div className="formTitleDiv" style={{ width: "100%" }}>
-              <h2 className="formTitle">Reports{"  "}</h2>
-              <hr />
-              <h2 className="formTitle">
-                <div
-                  style={{
-                    width: "100%",
-                    display: "flex",
-                    justifyContent: "space-evenly",
+  return !showForms && !showTrainings ? (
+    <div style={{ marginTop: "50px" }}>
+      <div className="row" style={{ margin: "0px 30px" }}>
+        <div className="formTitleDiv" style={{ width: "100%" }}>
+          <h2 className="formTitle">Reports{"  "}</h2>
+          <hr />
+          <h2 className="formTitle">
+            <div
+              style={{
+                width: "100%",
+                display: "flex",
+                justifyContent: "space-evenly",
+              }}
+            >
+              <button
+                className="btn btn-light extraInfoButton"
+                onClick={() => {
+                  showFormReports();
+                }}
+              >
+                Form Reports
+              </button>
+            </div>
+          </h2>
+          {isAdminUser(props.userObj) && (
+            <h2 className="formTitle">
+              <div
+                style={{
+                  width: "100%",
+                  display: "flex",
+                  justifyContent: "space-evenly",
+                }}
+              >
+                <button
+                  className="btn btn-light extraInfoButton"
+                  onClick={() => {
+                    showTrainingReports();
                   }}
                 >
-                  <button
-                    className="btn btn-light extraInfoButton"
-                    onClick={() => {
-                      this.showFormReports();
-                    }}
-                  >
-                    Form Reports
-                  </button>
-                </div>
-              </h2>
-              {isAdminUser(this.props.userObj) && (
-                <h2 className="formTitle">
-                  <div
-                    style={{
-                      width: "100%",
-                      display: "flex",
-                      justifyContent: "space-evenly",
-                    }}
-                  >
-                    <button
-                      className="btn btn-light extraInfoButton"
-                      onClick={() => {
-                        this.showTrainingReports();
-                      }}
-                    >
-                      Internal Trainings
-                    </button>
-                  </div>
-                </h2>
-              )}
-            </div>
-          </div>
-          <div className="reportBtnsMobile"></div>
+                  Internal Trainings
+                </button>
+              </div>
+            </h2>
+          )}
         </div>
-      );
-    } else {
-      if (this.state.showForms) {
-        return (
-          <div>
-            <FromReports
-              resetReports={() => {
-                this.resetReports();
-              }}
-              userObj={this.props.userObj}
-              allUsers={this.props.allUsers}
-            />
-          </div>
-        );
-      }
-      if (this.state.showTrainings) {
-        return (
-          <div>
-            <TrainingReports
-              resetReports={() => {
-                this.resetReports();
-              }}
-              userObj={this.props.userObj}
-              allUsers={this.props.allUsers}
-            />
-          </div>
-        );
-      }
-    }
-  }
-}
+      </div>
+      <div className="reportBtnsMobile"></div>
+    </div>
+  ) : showForms ? (
+    <div>
+      <FromReports
+        resetReports={() => {
+          resetReports();
+        }}
+        userObj={props.userObj}
+        allUsers={props.allUsers}
+      />
+    </div>
+  ) : (
+    <div>
+      <TrainingReports
+        resetReports={() => {
+          resetReports();
+        }}
+        userObj={props.userObj}
+        allUsers={props.allUsers}
+      />
+    </div>
+  );
+};
 
 export default ReportsContainer;
