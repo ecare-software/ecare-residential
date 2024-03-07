bd9f2ac71edb636536a5e0677c4cc5e3a368f0f6
annastasiablack <annastasiablack@gmail.com>
2024-03-06 22:37:37 -0600

    added table to report view, show incident report occurance date instead of created date

--- client/src/components/Forms/FormSubmitterListContainer.js
+++ client/src/components/Forms/FormSubmitterListContainer.js
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 
 const selectedUserNameClass = {
   color: "maroon",
-  padding: "10px",
+  // padding: "10px",
   borderRadius: "9px",
   cursor: "pointer",
   fontWeight: "800",
@@ -11,11 +11,19 @@ const selectedUserNameClass = {
 
 const userNameClass = {
   color: "black",
-  padding: "10px",
+  // padding: "10px",
   borderRadius: "9px",
   cursor: "pointer",
   fontWeight: "300",
 };
+
+const reportDateTimeClass = {
+  width: "20%"
+}
+
+const reportDetailsClass = {
+  width: "60%"
+}
 
 class FormSubmitterListContainer extends Component {
   constructor(props) {
@@ -56,26 +64,50 @@ class FormSubmitterListContainer extends Component {
               }
               key={formIndex}
             >
-              {/* Report View  */}
-              <div style={{ display: "flex", flexDirection: "row" }}>
-                <div className='col-sm-2'>
-                  {`${new Date(form.createDate).toLocaleDateString()}  `} 
-                </div>
-                <div className='col-sm-3'>
-                  {`${new Date(form.createDate).toLocaleTimeString('en-US', {hour: '2-digit', minute: '2-digit', hour12: true})}`} 
-                </div>
-                { this.state.formType === "Incident Report" ?
-                /* Incident Report Summary displayed upon button click */
-                <div className='col-sm-7'>
-                  {form.result === "" ? "None specified." : form.result}
-                </div>
-                :
-                /* Non-Incident Report Summary displayed upon button click */
-                <div className='col-sm-7'>
-                  {form.createdByName}
-                </div>
-  }
-              </div>           
+              <table class="table table-hover" style={{marginBottom:"0px", borderTopColor:"white"}}>
+                  {formIndex === 0 && (
+                     <thead style={{borderTopColor:"white"}}>
+                     <tr style={{borderTop:"white"}}>
+                        <th style={reportDateTimeClass} colspan="2">
+                        {this.state.formType === "Incident Report" ? "Incident Occured Date" : "Report Created Date"}
+                        </th>
+                        <th style={reportDetailsClass}>
+                          {this.state.formType === "Incident Report" ? "Incident Explanation" : "Reported By"}
+                        </th>
+                     </tr>
+                     </thead>
+                    )}               
+                <tbody>
+                  <tr>
+                    <td style={reportDateTimeClass}>
+                      {/* TODO: need to change format recorded on Incident Report to maintain consistency, or something else */}
+                    {this.state.formType === "Incident Report" ? form.dateOfIncident : `${new Date(form.createDate).toLocaleDateString()}`}
+                    </td>
+                    <td style={reportDateTimeClass}>
+                    {this.state.formType === "Incident Report" ? form.time_of_incident : `${new Date(form.createDate).toLocaleTimeString('en-US', {hour: '2-digit', minute: '2-digit', hour12: true})}`}
+                      {/* TODO: need to capture AM/PM recorded in Incident Report form */}
+                    </td>
+                    <td style={reportDetailsClass}>
+                      { this.state.formType === "Incident Report" ?
+                        /* Incident Report Summary displayed upon button click */
+                        <div>
+                          {form.result === "" ? "None specified." : form.result}
+                        </div>
+                        :
+                        /* Non-Incident Report Summary displayed upon button click */
+                        <div>
+                          {form.createdByName}
+                        </div>
+                      }
+                    </td>
+                  </tr>
+                </tbody>
+              </table>
+
+
+
+
+                      
               
             </li>
           ))
